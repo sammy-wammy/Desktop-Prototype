110cccf120750f4f4893f256d257aaf906bf72f5
trance-with-me <thequickandthedead@gmail.com>
2013-10-10 16:30:30 -0700

    Refactoring
    Fix dimensions of primary boundary box. Create tiers for alarm sound
    based on length of alarm sounding. Refactor code so alarm setting is
    not duplicated.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <Windows.h>
 
 #include <ctime>
 #include <iostream>
@@ -10,12 +11,13 @@
 using namespace std;
 using namespace cv;
 
-bool toggle_record(VideoWriter& record, int& record_counter, bool& record_to_file_a, Mat& capture_frame);
+bool toggle_record(VideoWriter& record, int& record_timer, bool& record_to_file_a, Mat& capture_frame);
+void sound_alarm(int tier);
+void reset_alarm(bool& alarm_flag, bool& alarm1, bool& alarm2, bool& alarm);
 
 // Globals
-const int COUNTER_SIZE = 50;
+const int timer_SIZE = 10;
 const int DELAY = 30;
-const double PADDING = 0.15;
 const int MAX_VIDEO_RECORDING_FRAMES = 60; // @ 15fps = 4 sec (testing for now)
 
 int main()
@@ -67,26 +69,30 @@ int main()
     const int line_type                     = 8;
     const int num_fract_bits_in_pnt_coords  = 0;
 
-    clock_t start, end, current;
+    clock_t start_alarm_time, end, current_alarm_time;
     
-    int counter         = 0; // too not discriptive
-    int video_frame_counter = 0;
-    int vertex2_x       = 0;
-    int vertex2_y       = 0;
-    int vertex1_x       = 0;
-    int vertex1_y       = 0;
-    int vertex2_x_avg   = 0;
-    int vertex2_y_avg   = 0;
-    int vertex1_x_avg   = 0;
-    int vertex1_y_avg   = 0;
-    int input           = 0;
+    int timer             = 0; 
+    int video_frame_timer = 0;
+    int vertex2_x         = 0;
+    int vertex2_y         = 0;
+    int vertex1_x         = 0;
+    int vertex1_y         = 0;
+    int vertex2_x_avg     = 0;
+    int vertex2_y_avg     = 0;
+    int vertex1_x_avg     = 0;
+    int vertex1_y_avg     = 0;
+    int input             = 0;
     
-    bool out_of_bounds  = false;
-    bool silenced       = false;
-    bool recalibrate    = false;
-    bool draw           = true;
-    bool input_ready    = false;
+    bool out_of_bounds    = false;
+    bool silenced         = false;
+    bool recalibrate      = false;
+    bool draw             = true;
+    bool input_ready      = false;
     bool record_to_file_a = true;
+    bool alarm_active     = false;
+    bool alarm1           = false;
+    bool alarm2           = false;
+    bool alarm3           = false;
 
     VideoWriter record("log_a.avi", CV_FOURCC('X','V','I','D'), 15, capture_frame.size(), true);
 
@@ -133,6 +139,7 @@ int main()
                 if (silenced) 
                 {
                     silenced = false;
+                    recalibrate = true;
                     cout << "silenced = false" << endl;
                     out_of_bounds = false;                    
                 }
@@ -171,24 +178,11 @@ int main()
         {
             if (faces.size() == 0)
             {
-                if (!out_of_bounds)
+                if (!alarm_active)
                 {
-                    start = clock();
-                    out_of_bounds = true;
+                    start_alarm_time = clock();
+                    alarm_active = true;
                 }    
-                // Difference in time (delta T)
-                current = clock() - start;
-                    
-                if ((double)current/CLOCKS_PER_SEC > 2)
-                {
-                    cout << "ALERT!" << endl;
-                    record << capture_frame;
-                    if (!toggle_record(record, video_frame_counter, record_to_file_a, capture_frame))
-                    {
-                        cerr << "Error: Recording subsequent video set" << endl;
-                        exit(1);
-                    }
-                }
             }
 
             // For all found faces in the vector, draw a rectangle on the original image
@@ -200,7 +194,7 @@ int main()
 
                 if (recalibrate)
                 {
-                    counter = 0;
+                    timer = 0;
                     recalibrate = false;
                     vertex1_x = 0;
                     vertex1_y = 0;
@@ -208,61 +202,52 @@ int main()
                     vertex2_y = 0;
                 }
            
-                if (counter < COUNTER_SIZE)
+                if (timer < timer_SIZE)
                 {
                     vertex1_x += faces[i].x;
                     vertex1_y += faces[i].y;
                     vertex2_x += faces[i].x + faces[i].width;
                     vertex2_y += faces[i].y + faces[i].height;
-                    counter++;
+                    timer++;
                 }
-                else {
+                else 
+                {
+                    int padding_width = static_cast<int>(0.15 * (vertex2_x_avg - vertex1_x_avg));
+                    int padding_top = static_cast<int>(0.05 * (vertex2_y_avg - vertex1_y_avg));
+                    int padding_bottom = static_cast<int>(0.2 * (vertex2_y_avg - vertex1_y_avg));
+
+                    vertex1_x_avg = vertex1_x/timer_SIZE - padding_width;
+                    vertex1_y_avg = vertex1_y/timer_SIZE - padding_top;
+
+                    vertex2_x_avg = vertex2_x/timer_SIZE + padding_width;
+                    vertex2_y_avg = vertex2_y/timer_SIZE + padding_bottom;
+
                     if (draw)
                     {
                         rectangle(capture_frame,
-                                  Point(vertex1_x/COUNTER_SIZE - (PADDING * (vertex1_x/COUNTER_SIZE)), 
-                                        vertex1_y/COUNTER_SIZE - (PADDING * (vertex1_y/COUNTER_SIZE))),
-                                  Point(vertex2_x/COUNTER_SIZE + (PADDING * (vertex2_x/COUNTER_SIZE)), 
-                                        vertex2_y/COUNTER_SIZE + (PADDING * (vertex2_y/COUNTER_SIZE))),
+                                  Point(vertex1_x_avg, vertex1_y_avg),
+                                  Point(vertex2_x_avg, vertex2_y_avg),
                                   cvScalar(0,255,0,100),
                                   rect_line_thickness,
                                   line_type,
                                   num_fract_bits_in_pnt_coords);   
                     } 
-                
-                    vertex1_x_avg = vertex1_x/COUNTER_SIZE - (PADDING * (vertex1_x/COUNTER_SIZE));
-                    vertex1_y_avg = vertex1_y/COUNTER_SIZE - (PADDING * (vertex1_y/COUNTER_SIZE));
 
-                    vertex2_x_avg = vertex2_x/COUNTER_SIZE + (PADDING * (vertex2_x/COUNTER_SIZE));
-                    vertex2_y_avg = vertex2_y/COUNTER_SIZE + (PADDING * (vertex2_y/COUNTER_SIZE));
-
+                    //Check out of bounds
                     if (faces[i].x < vertex1_x_avg
                         || faces[i].x + faces[i].width > vertex2_x_avg
                         || faces[i].y < vertex1_y_avg
                         || faces[i].y + faces[i].height > vertex2_y_avg)
                     {
-                        if (!out_of_bounds)
+                        if (!alarm_active)
                         {
-                            start = clock();
-                            out_of_bounds = true;
-                        }    
-                        // Difference in time (delta T)
-                        current = clock() - start;
-                    
-                        if ((double)current/CLOCKS_PER_SEC > 2)
-                        {
-                            cout << "ALERT!" << endl;
-                            record << capture_frame;
-                            if (!toggle_record(record, video_frame_counter, record_to_file_a, capture_frame))
-                            {
-                                cerr << "Error: Recording subsequent video set" << endl;
-                                exit(1);
-                            }
-                        }
+                            start_alarm_time = clock();
+                            alarm_active = true;
+                        }   
                     } 
                     else 
                     {
-                        out_of_bounds = false;
+                        reset_alarm(alarm_active, alarm1, alarm2, alarm3);
                     }
                 }
                 rectangle(capture_frame, 
@@ -273,9 +258,52 @@ int main()
                           line_type, 
                           num_fract_bits_in_pnt_coords); 
             }
+
+            record << capture_frame;
+            if (!toggle_record(record, video_frame_timer, record_to_file_a, capture_frame))
+            {
+                cerr << "Error: Recording subsequent video set" << endl;
+                exit(1);
+            }
+
+            if(alarm_active)
+            {
+                current_alarm_time = clock() - start_alarm_time;
+                cout << (double)current_alarm_time/CLOCKS_PER_SEC << " sec." << endl;
+
+                if ((double)current_alarm_time/CLOCKS_PER_SEC > 20)
+                {
+                    silenced = true;
+                }
+                else if ((double)current_alarm_time/CLOCKS_PER_SEC > 14)
+                {
+                    if(!alarm3)
+                    {
+                        alarm3 = true;
+                        sound_alarm(3);
+                    }
+                }
+                else if ((double)current_alarm_time/CLOCKS_PER_SEC > 8)
+                {
+                    if(!alarm2)
+                    {
+                        alarm2 = true;
+                        sound_alarm(2);
+                    }
+                }
+                else if ((double)current_alarm_time/CLOCKS_PER_SEC > 2)
+                {
+                    if(!alarm1)
+                    {
+                        alarm1 = true;
+                        sound_alarm(1);
+                    }
+                }
+            }
         }
-        else 
+        else //end of !SILENCED
         {
+            reset_alarm(alarm_active, alarm1, alarm2, alarm3);
             input = waitKey(DELAY);
         }
         // Print the output
@@ -285,11 +313,11 @@ int main()
     return 0;
 }
 
-bool toggle_record(VideoWriter& record, int& record_counter, bool& record_to_file_a, Mat& capture_frame)
+bool toggle_record(VideoWriter& record, int& record_timer, bool& record_to_file_a, Mat& capture_frame)
 {
-    if (record_counter > MAX_VIDEO_RECORDING_FRAMES)
+    if (record_timer > MAX_VIDEO_RECORDING_FRAMES)
     {
-        record_counter = 0;
+        record_timer = 0;
         record.release();
 
         if (record_to_file_a == true)
@@ -307,5 +335,37 @@ bool toggle_record(VideoWriter& record, int& record_counter, bool& record_to_fil
             exit(1);
         }
     }
-    record_counter++;
+    record_timer++;
+}
+
+void sound_alarm(int tier)
+{
+    switch(tier)
+    {
+        case 1:
+            cout << "ALERT 1!" << endl;
+            cout << '\a' << endl;
+            break;
+
+        case 2:
+            cout << "ALERT 2!" << endl;
+            cout << '\a' << '\a' << endl;
+            break;
+
+        case 3:
+            cout << "ALERT 3!" << endl;
+            cout << '\a' << '\a' << '\a' << endl;
+            break;
+
+        default:
+            break;
+    }
+}
+
+void reset_alarm(bool& alarm_flag, bool& alarm1, bool& alarm2, bool& alarm3)
+{
+    alarm_flag = false;
+    alarm1 = false;
+    alarm2 = false;
+    alarm3 = false;
 }
